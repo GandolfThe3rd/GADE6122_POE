cb256ccbbd6d1909837155066b1dba6380ead7ca
GandolfThe3rd <ethanram3110@gmail.com>
2024-09-06 04:54:39 -0700

    Compleated 2.1 and 2.2

---
+++ Tile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hero_Adventure
+{
+    public abstract class Tile
+    {
+        private Position position;
+
+        public int X
+        {
+            get { return position.X; }
+            set { position.X = value; }
+        }
+
+        public int Y
+        {
+            get { return position.X; }
+            set { position.X = value; }
+        }
+
+        public Tile(Position aPosition)
+        {
+            position = aPosition;
+        }
+
+        public abstract char Display
+        {
+            get;
+        }
+    }
+}
